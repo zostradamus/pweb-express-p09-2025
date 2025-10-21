49b11fd1b00c68af7e46ec9d8a989f745bad2df3
idzaanafin <ahmadidanafin@gmail.com>
2025-10-21 21:40:34 +0700

    setup

---
+++ prisma/migrations/20251021133535_init/migration.sql
@@ -0,0 +1,83 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "genres" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "genres_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "books" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "writer" TEXT NOT NULL,
+    "publisher" TEXT NOT NULL,
+    "publication_year" INTEGER NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "stock_quantity" INTEGER NOT NULL,
+    "genre_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order_items" (
+    "id" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "order_id" TEXT NOT NULL,
+    "book_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "order_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "genres_name_key" ON "genres"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "books_title_key" ON "books"("title");
+
+-- AddForeignKey
+ALTER TABLE "books" ADD CONSTRAINT "books_genre_id_fkey" FOREIGN KEY ("genre_id") REFERENCES "genres"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import prisma from './config/prisma';
+
+// import authRoutes from './routes/authRoutes';
+// import genreRoutes from './routes/genreRoutes';
+// import libraryRoutes from './routes/libraryRoutes';
+// import transactionRoutes from './routes/transactionRoutes';
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test route
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Express + Prisma + PostgreSQL' });
+});
+
+// Import routes
+// app.use('/api/auth', authRoutes);
+// app.use('/api/genres', genreRoutes);
+// app.use('/api/libraries', libraryRoutes);
+// app.use('/api/transactions', transactionRoutes);
+
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ error: 'Endpoint tidak ditemukan' });
+});
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
+
+// Graceful shutdown
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  console.log('Database disconnected');
+  process.exit(0);
+});
